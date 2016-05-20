14310ade4d3fce02fc7723a17e3b3507479908f3
Lorkiyn <dari18@online.de>
2016-05-20 14:08:59 +0200

    jQuerry changes, css in extra folder and menu fixes

--- js/script.js
+++ js/script.js
@@ -4,9 +4,13 @@ $(document).ready(function() {
 
 		$("li").each(function(item) {
 			$(this).removeClass("markiert");
+			$(this).find("a").removeClass("markiert");
+
 		});
 
+		$(this).find("a").addClass("markiert");
 		$(this).addClass("markiert");
+		$(this).css('color', 'red');
 
 	});
 });
