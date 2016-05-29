4e91d093c96e8eb59433df6fcbaff04a21a569e1
Slajoc <gitundso@web.de>
2016-05-29 17:27:59 +0200

    fixed height of subsites, news edit

--- js/script.js
+++ js/script.js
@@ -8,6 +8,54 @@ $(document).ready(function() {
 
 		});
 
+		$(".active").click(function(){
+        var newHeight = $(".input-height").val();
+        $(".indexContent").height("300");
+
+    	});
+
+		$(".news").click(function(){
+        var newHeight = $(".input-height").val();
+        $(".indexContent").height("1000");
+
+    	});
+
+    	$(".history").click(function(){
+        var newHeight = $(".input-height").val();
+        $(".indexContent").height("1050");
+
+    	});
+
+    	$(".rules").click(function(){
+        var newHeight = $(".input-height").val();
+        $(".indexContent").height("500");
+
+    	});
+
+    	$(".windsurf").click(function(){
+        var newHeight = $(".input-height").val();
+        $(".indexContent").height("750");
+
+    	});
+
+    	$(".story").click(function(){
+        var newHeight = $(".input-height").val();
+        $(".indexContent").height("600");
+
+    	});
+
+    	$(".picture").click(function(){
+        var newHeight = $(".input-height").val();
+        $(".indexContent").height("1050");
+
+    	});
+
+    	$(".info").click(function(){
+        var newHeight = $(".input-height").val();
+        $(".indexContent").height("850");
+
+    	});
+
 		$(this).find("a").addClass("active");
 		$(this).addClass("active");
 
